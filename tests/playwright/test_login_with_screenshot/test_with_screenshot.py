7984093a2efbb5b8cd8c95627b887490f32f7828
José GUilherme <jose.guilherme96@outlook.com.br>
2026-01-19 12:42:26 -0300

    test: add example playwright tests

---
+++ tests/playwright/test_login_with_screenshot/test_with_screenshot.py
@@ -0,0 +1,21 @@
+import pytest
+import pytest
+import re
+from playwright.sync_api import Page, expect
+
+@pytest.mark.login
+def test_login_with_screenshot(page: Page):
+
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+
+    page.get_by_label("username").fill("student")
+    page.get_by_label("password").fill("Password123")
+
+
+    page.get_by_role("button", name="submit").click()
+
+    expect(page.get_by_text(re.compile("Congratulations"))).to_be_visible()
+
+    expect(page.get_by_text(re.compile("Log out"))).to_be_visible()
+
+    page.screenshot(path="tests/playwright/test_login_with_screenshot/test_with_screenshot.png")
---
+++ tests/playwright/test_with_generate_automatic_code.py
@@ -0,0 +1,33 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    expect(page.locator("h2")).to_contain_text("Test login")
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("student")
+    page.get_by_role("button", name="Submit").click()
+    page.locator("#error").click()
+    expect(page.locator("#error")).to_contain_text("Your password is invalid!")
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("student")
+    page.get_by_text("Password123").first.click()
+    page.get_by_role("textbox", name="Password").click(modifiers=["Shift"])
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill("Password123")
+    page.get_by_role("button", name="Submit").click()
+    expect(page.get_by_role("article")).to_contain_text("Log out")
+    page.get_by_role("link", name="Log out").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+if __name__ == '__main__':
+
+    with sync_playwright() as playwright:
+        test_run(playwright)
