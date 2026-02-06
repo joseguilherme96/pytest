de570a30a21c3d3c224b0b459bc424a4ddfb034d
José GUilherme <jose.guilherme96@outlook.com.br>
2026-02-06 14:03:32 -0300

    fix: adiciona formatação de codigo com `ruff`

--- src/playwright_selenium_tox_example/src/preenchimento_automatico_formulario/selenium_execution/page/contact_page.py
+++ src/playwright_selenium_tox_example/src/preenchimento_automatico_formulario/selenium_execution/page/contact_page.py
@@ -2,27 +2,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class ContactPage:
 
-    def __init__(self,driver):
+class ContactPage:
+    def __init__(self, driver):
 
         self.driver = driver
 
-    def open_page(self,url):
+    def open_page(self, url):
 
         self.driver.get(url)
 
     def enter_username(self, username):
 
-        self.driver.find_element(By.ID, "login").send_keys(
-            username
-        )
+        self.driver.find_element(By.ID, "login").send_keys(username)
 
     def enter_password(self, password):
 
-        self.driver.find_element(By.ID, "senha").send_keys(
-            password
-        )
+        self.driver.find_element(By.ID, "senha").send_keys(password)
 
     def enter_name(self, name):
 
@@ -30,15 +26,10 @@ class ContactPage:
 
     def enter_last_name(self, last_name):
 
-        self.driver.find_element(By.ID,"wpforms-161-field_0-last").send_keys(last_name)
+        self.driver.find_element(By.ID, "wpforms-161-field_0-last").send_keys(last_name)
 
     def wait_iframe(self):
 
         WebDriverWait(self.driver, 10).until(
             EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME, "iframe"))
         )
-
-    
-
-
-    
--- src/playwright_selenium_tox_example/tests/selenium/test_selenium.py
+++ src/playwright_selenium_tox_example/tests/selenium/test_selenium.py
@@ -1,17 +1,6 @@
-
 from preenchimento_automatico_formulario.selenium_execution.run import navegator
+
 
 def test_navegator_page(chrome_browser):
 
     navegator(chrome_browser)
-    
-
-
-
-
-
-
-
-
-    
-
